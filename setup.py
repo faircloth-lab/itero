7adf26a7d9ab19ebd90d80d81410ac94f3059e1d
Brant Faircloth <brant@faircloth-lab.org>
2018-04-14 15:11:02 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+#from distutils.core import setup
+from setuptools import setup
+
+
+def package_files(directory):
+    """package up files.  from stackoverflow.com
+    how-to-add-package-data-recursively-in-python-setup-py"""
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+setup(
+    name='itero',
+    version='1.0.0',
+    description='a strongly opinionated, iterative, guided assembly pipeline',
+    url='https://github.com/faircloth-lab/itero',
+    author='Brant C. Faircloth',
+    author_email='borg@faircloth-lab.org',
+    license='MIT',
+    platforms='any',
+    packages=[
+        'itero', 'itero.assemble', 'itero.cli'
+    ],
+    data_files=[('config', ['itero/config/itero.conf'])],
+    scripts=['bin/itero'],
+    package_data={'': package_files('itero/tests')},
+    install_requires=['numpy', 'schwimmbad', 'biopython', 'six'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+)
